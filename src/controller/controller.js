0a108f80eef6e9692872cace8a3e01fe853365af
Igor Suvorov <hi@isuvorov.ru>
2017-06-03 17:49:04 +0400

    fix

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,107 +1,73 @@
 import _ from 'lodash';
 
-// const sample = {
-//   avatar: '/assets/no-avatar.png',
-//   fullName: 'Счастливый Пользователь',
-// };
-// function fullName(user) {
-//   let fullname;
-//   if (user.profile.middleName) {
-//     fullname = [user.profile.lastName, user.profile.firstName, user.profile.middleName];
-//   } else {
-//     fullname = [user.profile.firstName, user.profile.lastName];
-//   }
-//   return fullname.filter(a => a).join(' ') || sample.fullName;
-// }
-
-
 export default (ctx) => {
   const { e404 } = ctx.errors;
-  const { checkNotFound } = ctx.helpers;
   const controller = {};
+  const { checkNotFound, createResourse, wrapResoursePoint } = ctx.helpers;
 
-  controller.list = async (req) => {
-    const { User } = ctx.models;
-    // return User.findByParams(req.data)
-    const params = req.allParams();
-    const { query } = params;
-    let { limit = undefined, offset = 0 } = params;
-    if (offset) offset = parseInt(offset, 10);
-    if (limit) limit = parseInt(limit, 10);
-    const users = await User
-      .find(query)
-      .limit(limit)
-      .skip(offset);
-    return users;
+  // console.log({User}, User.findByParams);
+
+  // const baseResourse = createResourse(User);
+  const resourse = {
+    // ...baseResourse,
+
+    async list(req) {
+      const { User } = ctx.models;
+      return User.findByParams(req.data);
+    },
+
+    //  async length () {
+    //   const { User } = ctx.models;
+    //   const count = await User.count({});
+    //   return { count };
+    // };
+
+    async me(req) {
+      const { User } = ctx.models;
+      const user = await User.findOne({ _id: req.user._id });
+      if (!user) throw e404('User not found!');
+      return user;
+    },
+
+    async get(req) {
+      const { User } = ctx.models;
+      const criteria = req.data;
+
+      const user = await User.findOne(criteria);
+      if (!user) throw e404('User not found!');
+      return user;
+    },
+
+    async update(req) {
+      const { User } = ctx.models;
+      const userId = req.user._id;
+      if (!userId) throw e404('userId not found!');
+      const params = req.data;
+
+      const user = await User.findById(userId);
+      if (!user) throw e404('User not found!');
+      if (params.username) user.username = params.username;
+      if (params.password) user.password = params.password;
+      user.profile = _.merge({}, user.profile, params.profile);
+      return user.save();
+    },
+    async edit(req) {
+      const { User } = ctx.models;
+      const userId = req.user._id;
+      if (!userId) throw e404('userId not found!');
+      const params = req.data;
+
+      const user = await User.findById(userId);
+      if (!user) throw e404('User not found!');
+      if (params.username) user.username = params.username;
+      if (params.password) user.password = params.password;
+      user.profile = _.merge({}, user.profile, params.profile);
+      return user.save();
+    },
+
+    create: undefined,
+    remove: undefined,
   };
 
-  controller.length = async () => {
-    const { User } = ctx.models;
-
-    const count = await User.count({});
-    return { count };
-  };
-
-  controller.me = async (req) => {
-    const { User } = ctx.models;
-
-    const user = await User.findOne({ _id: req.user._id });
-    if (!user) throw e404('User not found!');
-    return user;
-  };
-
-  controller.get = async (req) => {
-    const { User } = ctx.models;
-    const criteria = req.allParams();
-
-    const user = await User.findOne(criteria);
-    if (!user) throw e404('User not found!');
-    return user;
-  };
-
-
-  controller.edit = async (req) => {
-    const { User } = ctx.models;
-    // try {
-    const userId = req.user._id;
-    if (!userId) throw e404('userId not found!');
-    const params = req.allParams();
-
-    const user = await User.findById(userId);
-    if (!user) throw e404('User not found!');
-    // ctx.log.info('params', params);
-    // ctx.log.info('user', user);
-    // _.merge(user, _.pick(params, ['username', 'password', 'profile']));
-    if (params.username) user.username = params.username;
-    if (params.password) user.password = params.password;
-    user.profile = _.merge({}, user.profile, params.profile);
-    // user.name = fullName(user);
-
-    return user.save();
-    // } catch (error) {
-    //   throw e500(error);
-    // }
-  };
-  //
-  // controller.updateSocialData = async (req) => {
-  //   const user = await User
-  //   .findById(req.user._id)
-  //   .then(checkNotFound);
-  //   const passport = await Passport.findOne({
-  //     user: user._id,
-  //     provider: 'youtube',
-  //   });
-  //   await passport.updateSocialData();
-  //   return passport.save();
-  // };
-  // controller.getSocialData = async (req) => {
-  //   const user = await User
-  //   .findById(req.user._id)
-  //   .then(checkNotFound);
-  //   const socialData = await user.getSocialData(req.data.provider);
-  //   if (socialData) return socialData;
-  //   throw e404('!passport.meta');
-  // };
-
-  return controller;
+  return resourse;
 };
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 export default ctx => (
   {
     async init() {
-      this.models = require('./models').default(ctx, this);
+      // this.models = require('./models').default(ctx, this);
       this.controller = require('./controller').default(ctx, this);
     },
 
@@ -12,14 +12,9 @@ export default ctx => (
 
     getApi() {
       const api = ctx.asyncRouter();
-      const { isAuth } = ctx.middlewares;
+      const { wrapResoursePoint } = ctx.helpers;
 
-      api.all('/me', isAuth, this.controller.me);
-      api.all('/list', this.controller.list);
-      api.all('/length', this.controller.length);
-      api.all('/get', this.controller.get);
-      api.all('/edit', this.controller.edit);
-      api.all('/update', this.controller.update);
+      api.use('/', wrapResoursePoint(this.controller));
 
       return api;
     },

1de16dc70e0474e2d2f79e80b81e458109997377
Igor Suvorov <hi@isuvorov.ru>
2017-09-26 09:56:59 +0300

    add User.prepare for all operations

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -22,20 +22,14 @@ export default (ctx) => {
       if (online != null) {
         return users.filter(user => user.online == online);
       }
-      return users
+      return User.prepare(users, { req });
     },
-
-    //  async length () {
-    //   const { User } = ctx.models;
-    //   const count = await User.count({});
-    //   return { count };
-    // };
 
     async me(req) {
       const { User } = ctx.models;
       const user = await User.findOne({ _id: req.user._id });
       if (!user) throw e404('User not found!');
-      return user;
+      return User.prepare(user, { req });
     },
 
     async get(req) {
@@ -44,7 +38,7 @@ export default (ctx) => {
 
       const user = await User.findOne(criteria);
       if (!user) throw e404('User not found!');
-      return user;
+      return User.prepare(user, { req });
     },
 
     async update(req) {
@@ -57,22 +51,12 @@ export default (ctx) => {
       if (!user) throw e404('User not found!');
       if (params.username) user.username = params.username;
       if (params.password) user.password = params.password;
-      user.profile = _.merge({}, user.profile, params.profile);
-      return user.save();
-    },
-    async edit(req) {
-      const { User } = ctx.models;
-      const userId = req.user._id;
-      if (!userId) throw e404('userId not found!');
-      const params = req.data;
-
-      const user = await User.findById(userId);
-      if (!user) throw e404('User not found!');
-      if (params.username) user.username = params.username;
-      if (params.password) user.password = params.password;
       _.merge(user.profile, params.profile);
       user.markModified('profile');
-      return user.save();
+      return User.prepare(user, { req });
+    },
+    async edit(...args) {
+      return resourse.update(...args);
     },
   };
 
